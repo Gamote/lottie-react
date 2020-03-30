7fd81da614204b1ba6ccc8efe5e0d927f1c9c83f
Gamote <contact@gamote.ro>
2020-03-31 00:53:46 +0200

    Fix for assets paths on GitHub Pages

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,7 @@
 export default {
 	src: "docs",
 	dest: "docs-dist",
+	base: "/lottie-react/", // GitHub Pages sub-path
 	ignore: ["README.md"],
 	title: "Lottie wrapper for React",
 	themeConfig: {
