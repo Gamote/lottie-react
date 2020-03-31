1feb4d1a57433b9b86a27eaab7228a0624484b33
Gamote <contact@gamote.ro>
2020-03-31 14:38:02 +0200

    Added comment with the methods that need to be described in the documentation

--- docs/use-lottie/BasicUsage.js
+++ docs/use-lottie/BasicUsage.js
@@ -13,10 +13,28 @@ const BasicUsage = () => {
 		loop: true,
 		autoplay: true,
 	};
+	
+	const Lottie = useLottie(options, style);
 
-	const { View } = useLottie(options, style);
-
-	return <>{View}</>;
+	// TODO: methods to describe in the documentation
+	// useEffect(() => {
+	// 	setTimeout(() => {
+	// 		// Lottie.play();
+	// 		// Lottie.stop();
+	// 		// Lottie.pause();
+	// 		// Lottie.setSpeed(5);
+	// 		// Lottie.goToAndStop(6150);
+	// 		// Lottie.goToAndPlay(6000);
+	// 		// Lottie.setDirection(-1);
+	// 		// Lottie.playSegments([350, 500]);
+	// 		// Lottie.playSegments([350, 500], true);
+	// 		// Lottie.setSubframe(true);
+	// 		// Lottie.destroy();
+	// 		// console.log('Duration:', Lottie.getDuration());
+	// 	}, 2000);
+	// });
+	
+	return <>{Lottie.View}</>;
 };
 
 export default BasicUsage;
