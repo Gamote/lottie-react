4852da7c246709f0b33cbda65f5c1cfa00f7c2d0
Gamote <contact@gamote.ro>
2020-03-31 03:24:40 +0200

    Added comments to describe what to implement next & changing some of them that were already created

--- src/hooks/useLottie.js
+++ src/hooks/useLottie.js
@@ -5,9 +5,7 @@ const useLottie = (props, style = {}) => {
 	const animationInstanceRef = useRef(null);
 	const animationContainer = useRef(null);
 
-	/**
-	 * Setup LottieAnimator
-	 */
+	// Initialize on mount
 	useEffect(() => {
 		// Instantiate Lottie
 		animationInstanceRef.current = lottie.loadAnimation({
@@ -21,19 +19,52 @@ const useLottie = (props, style = {}) => {
 		};
 		// eslint-disable-next-line
 	}, []);
-
+	
+	// TODO: play
+	
+	// TODO: stop
+	
+	// TODO: pause
+	
+	// TODO: pause
+	
+	/**
+	 * Set animation speed
+	 * TODO: complete
+	 * @param speed
+	 */
 	const setSpeed = speed => {
 		if (animationInstanceRef.current) {
 			animationInstanceRef.current.setSpeed(speed);
 		}
 	};
-
+	
+	// TODO: goToAndStop
+	
+	// TODO: goToAndPlay
+	
+	// TODO: setDirection
+	
+	// TODO: setDirection
+	
+	/**
+	 * TODO: complete
+	 * @param segment
+	 * @param force
+	 */
 	const playSegments = (segment, force = false) => {
 		if (animationInstanceRef.current) {
 			animationInstanceRef.current.playSegments(segment, force);
 		}
 	};
+	
+	// TODO: setSubframe
+	
+	// TODO: destroy
+	
+	// TODO: getDuration
 
+	// Build the animation view
 	const View = <div style={style} ref={animationContainer} />;
 
 	return {
