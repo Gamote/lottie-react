ff9ade78a4e46a6f8cd6e9316f92bfd675d07604
Gamote <contact@gamote.ro>
2020-04-09 22:57:24 +0200

    Share methods instead of the full lottie instance to the parent through the ref object - this way we can control the cases in which a an instance method will be call before the animation is instantiated

--- src/components/Animator.js
+++ src/components/Animator.js
@@ -8,7 +8,132 @@ const Animator = forwardRef((props, ref) => {
 	const animationInstanceRef = useRef(null);
 	const parentRef = ref || useRef();
 
-	// Helper that load a new animation, and if it's the case, destroy the previous one
+	/**
+	 * Play
+	 * TODO: complete
+	 */
+	const play = () => {
+		if (animationInstanceRef.current) {
+			animationInstanceRef.current.play();
+		}
+	};
+
+	/**
+	 * Stop
+	 * TODO: complete
+	 */
+	const stop = () => {
+		if (animationInstanceRef.current) {
+			animationInstanceRef.current.stop();
+		}
+	};
+
+	/**
+	 * Pause
+	 * TODO: complete
+	 */
+	const pause = () => {
+		if (animationInstanceRef.current) {
+			animationInstanceRef.current.pause();
+		}
+	};
+
+	/**
+	 * Set animation speed
+	 * TODO: complete
+	 * @param speed
+	 */
+	const setSpeed = speed => {
+		if (animationInstanceRef.current) {
+			animationInstanceRef.current.setSpeed(speed);
+		}
+	};
+	
+	/**
+	 * Got to frame and play
+	 * TODO: complete
+	 * @param value
+	 * @param isFrame
+	 */
+	const goToAndPlay = (value, isFrame) => {
+		if (animationInstanceRef.current) {
+			animationInstanceRef.current.goToAndPlay(value, isFrame);
+		}
+	};
+
+	/**
+	 * Got to frame and stop
+	 * TODO: complete
+	 * @param value
+	 * @param isFrame
+	 */
+	const goToAndStop = (value, isFrame) => {
+		if (animationInstanceRef.current) {
+			animationInstanceRef.current.goToAndStop(value, isFrame);
+		}
+	};
+
+	/**
+	 * Set animation direction
+	 * TODO: complete
+	 * @param direction
+	 */
+	const setDirection = direction => {
+		if (animationInstanceRef.current) {
+			animationInstanceRef.current.setDirection(direction);
+		}
+	};
+
+	/**
+	 * Play animation segments
+	 * TODO: complete
+	 * @param segment
+	 * @param force
+	 */
+	const playSegments = (segment, force) => {
+		if (animationInstanceRef.current) {
+			animationInstanceRef.current.playSegments(segment, force);
+		}
+	};
+
+	/**
+	 * Set sub frames
+	 * TODO: complete
+	 * @param useSubFrames
+	 */
+	const setSubframe = useSubFrames => {
+		if (animationInstanceRef.current) {
+			animationInstanceRef.current.setSubframe(useSubFrames);
+		}
+	};
+
+	/**
+	 * Destroy animation
+	 * TODO: complete
+	 */
+	const destroy = () => {
+		if (animationInstanceRef.current) {
+			animationInstanceRef.current.destroy();
+		}
+	};
+
+	/**
+	 * Get animation duration
+	 * TODO: complete
+	 * @param inFrames
+	 */
+	const getDuration = inFrames => {
+		if (animationInstanceRef.current) {
+			return animationInstanceRef.current.getDuration(inFrames);
+		}
+
+		return undefined;
+	};
+
+	/**
+	 * Load a new animation, and if it's the case, destroy the previous one
+	 * @param forceOptions
+	 */
 	const loadAnimation = (forceOptions = {}) => {
 		if (animationInstanceRef.current) {
 			animationInstanceRef.current.destroy();
@@ -26,9 +151,21 @@ const Animator = forwardRef((props, ref) => {
 
 		animationInstanceRef.current = lottie.loadAnimation(config);
 
-		// Share lottie instance to the parent
+		// Share methods which control Lottie to the parent component
 		if (parentRef) {
-			parentRef.current = animationInstanceRef.current;
+			parentRef.current = {
+				play,
+				stop,
+				pause,
+				setSpeed,
+				goToAndPlay,
+				goToAndStop,
+				setDirection,
+				playSegments,
+				setSubframe,
+				destroy,
+				getDuration,
+			};
 		}
 	};
 
@@ -37,6 +174,9 @@ const Animator = forwardRef((props, ref) => {
 		loadAnimation();
 	}, [animationData, loop, autoplay, initialSegment]);
 
+	/**
+	 * ALPHA
+	 */
 	// Detect changes of the loop param and change it without reloading the animation
 	// TODO: needs intensive testing
 	// useEffect(() => {
