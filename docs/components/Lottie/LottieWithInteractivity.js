d1e1419b6c5ce8a4a43850d9e15523f4d090f415
shelooks16 <blauyedz@zoho.eu>
2020-07-08 20:06:49 +0300

    adjust on scroll example to v2.0.0

--- docs/components/Lottie/LottieWithInteractivity.js
+++ docs/components/Lottie/LottieWithInteractivity.js
@@ -3,73 +3,71 @@ import Lottie from "../../../src/components/Lottie";
 import groovyWalkAnimation from "../../assets/groovyWalk.json";
 
 const style = {
-	height: 300,
-	border: 3,
-	borderStyle: "solid",
-	borderRadius: 7,
+  height: 300,
+  border: 3,
+  borderStyle: "solid",
+  borderRadius: 7,
 };
 
 const viewport = [0, 1];
 
 function getContainerVisibility(container) {
-	const { top, height } = container.getBoundingClientRect();
+  const { top, height } = container.getBoundingClientRect();
 
-	const current = window.innerHeight - top;
-	const max = window.innerHeight + height;
-	return current / max;
+  const current = window.innerHeight - top;
+  const max = window.innerHeight + height;
+  return current / max;
 }
 
 const LottieWithInteractivity = () => {
-	const containerRef = useRef();
-	const lottieRef = useRef();
+  const containerRef = useRef();
+  const lottieRef = useRef();
 
-	useEffect(() => {
-		if (containerRef.current && lottieRef.current) {
-			const container = containerRef.current;
-			const { getDuration, goToAndStop } = lottieRef.current;
+  useEffect(() => {
+    if (containerRef.current && lottieRef.current) {
+      const container = containerRef.current;
+      const { getDuration, goToAndStop } = lottieRef.current;
 
-			const totalFrames = getDuration(true);
+      const totalFrames = getDuration(true);
 
-			const scrollHandler = () => {
-				const currentPercent = getContainerVisibility(container);
+      const scrollHandler = () => {
+        const currentPercent = getContainerVisibility(container);
 
-				const isInView =
-					currentPercent >= viewport[0] &&
-					currentPercent <= viewport[1];
+        const isInView =
+          currentPercent >= viewport[0] && currentPercent <= viewport[1];
 
-				if (isInView) {
-					goToAndStop(
-						Math.ceil(
-							((currentPercent - viewport[0]) /
-								(viewport[1] - viewport[0])) *
-								totalFrames,
-						),
-						true,
-					);
-				}
-			};
+        if (isInView) {
+          goToAndStop(
+            Math.ceil(
+              ((currentPercent - viewport[0]) / (viewport[1] - viewport[0])) *
+                totalFrames,
+            ),
+            true,
+          );
+        }
+      };
 
-			document.addEventListener("scroll", scrollHandler);
+      document.addEventListener("scroll", scrollHandler);
 
-			return () => {
-				document.removeEventListener("scroll", scrollHandler);
-			};
-		}
+      return () => {
+        document.removeEventListener("scroll", scrollHandler);
+      };
+    }
 
-		return () => {};
-	}, [containerRef.current, lottieRef.current]);
+    return () => {};
+  }, [containerRef.current, lottieRef.current]);
 
-	return (
-		<div ref={containerRef}>
-			<Lottie
-				ref={lottieRef}
-				animationData={groovyWalkAnimation}
-				style={style}
-				loop={false}
-				autoplay={false}
-			/>
-		</div>
-	);
+  return (
+    <div ref={containerRef}>
+      <Lottie
+        lottieRef={lottieRef}
+        animationData={groovyWalkAnimation}
+        style={style}
+        loop={false}
+        autoplay={false}
+      />
+    </div>
+  );
 };
 
 export default LottieWithInteractivity;
--- docs/hooks/useLottie/UseLottieWithInteractivity.js
+++ docs/hooks/useLottie/UseLottieWithInteractivity.js
@@ -3,67 +3,65 @@ import useLottie from "../../../src/hooks/useLottie";
 import groovyWalkAnimation from "../../assets/groovyWalk.json";
 
 const style = {
-	height: 300,
-	border: 3,
-	borderStyle: "solid",
-	borderRadius: 7,
+  height: 300,
+  border: 3,
+  borderStyle: "solid",
+  borderRadius: 7,
 };
 
 const viewport = [0, 1];
 
 const options = {
-	animationData: groovyWalkAnimation,
-	loop: false,
-	autoplay: false,
+  animationData: groovyWalkAnimation,
+  loop: false,
+  autoplay: false,
 };
 
 function getContainerVisibility(container) {
-	const { top, height } = container.getBoundingClientRect();
+  const { top, height } = container.getBoundingClientRect();
 
-	const current = window.innerHeight - top;
-	const max = window.innerHeight + height;
-	return current / max;
+  const current = window.innerHeight - top;
+  const max = window.innerHeight + height;
+  return current / max;
 }
 
 const UseLottieWithInteractivity = () => {
-	const { View, getDuration, goToAndStop } = useLottie(options, style);
-	const containerRef = useRef();
+  const { View, getDuration, goToAndStop } = useLottie(options, style);
+  const containerRef = useRef();
 
-	useEffect(() => {
-		if (containerRef.current) {
-			const totalFrames = getDuration(true);
-			const container = containerRef.current;
+  useEffect(() => {
+    if (containerRef.current) {
+      const totalFrames = getDuration(true);
+      const container = containerRef.current;
 
-			const scrollHandler = () => {
-				const currentPercent = getContainerVisibility(container);
+      const scrollHandler = () => {
+        const currentPercent = getContainerVisibility(container);
 
-				const isInView =
-					currentPercent >= viewport[0] &&
-					currentPercent <= viewport[1];
+        const isInView =
+          currentPercent >= viewport[0] && currentPercent <= viewport[1];
 
-				if (isInView) {
-					goToAndStop(
-						Math.ceil(
-							((currentPercent - viewport[0]) /
-								(viewport[1] - viewport[0])) *
-								totalFrames,
-						),
-						true,
-					);
-				}
-			};
+        if (isInView) {
+          goToAndStop(
+            Math.ceil(
+              ((currentPercent - viewport[0]) / (viewport[1] - viewport[0])) *
+                totalFrames,
+            ),
+            true,
+          );
+        }
+      };
 
-			document.addEventListener("scroll", scrollHandler);
+      document.addEventListener("scroll", scrollHandler);
 
-			return () => {
-				document.removeEventListener("scroll", scrollHandler);
-			};
-		}
+      return () => {
+        document.removeEventListener("scroll", scrollHandler);
+      };
+    }
 
-		return () => {};
-	}, [goToAndStop, containerRef.current]);
+    return () => {};
+  }, [goToAndStop, containerRef.current]);
 
-	return <div ref={containerRef}>{View}</div>;
+  return <div ref={containerRef}>{View}</div>;
 };
 
 export default UseLottieWithInteractivity;
