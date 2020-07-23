fb3e703e517fd9387e4d8667b57d7f4a7776c411
shelooks16 <blauyedz@zoho.eu>
2020-07-23 14:34:38 +0300

    add docs for interactivity /w examples

---
+++ docs/hooks/useLottieInteractivity/CursorHorizontalSync.js
@@ -0,0 +1,38 @@
+import useLottie from "../../../src/hooks/useLottie";
+import useLottieInteractivity from "../../../src/hooks/useLottieInteractivity";
+import hamsterAnimation from "../../assets/hamster.json";
+
+const style = {
+  height: 300,
+  border: 3,
+  borderStyle: "solid",
+  borderRadius: 7,
+};
+
+const options = {
+  animationData: hamsterAnimation,
+};
+
+const CursorHorizontalSync = () => {
+  const lottieObj = useLottie(options, style);
+  const Animation = useLottieInteractivity({
+    lottieObj,
+    mode: "cursor",
+    actions: [
+      {
+        position: { x: [0, 1], y: [-1, 2] },
+        type: "seek",
+        frames: [0, 179],
+      },
+      {
+        position: { x: -1, y: -1 },
+        type: "stop",
+        frames: [0],
+      },
+    ],
+  });
+
+  return Animation;
+};
+
+export default CursorHorizontalSync;
---
+++ docs/hooks/useLottieInteractivity/ScrollWithOffsetAndLoop.js
@@ -0,0 +1,42 @@
+import useLottie from "../../../src/hooks/useLottie";
+import useLottieInteractivity from "../../../src/hooks/useLottieInteractivity";
+import robotAnimation from "../../assets/robotAnimation.json";
+
+const style = {
+  height: 450,
+};
+
+const options = {
+  animationData: robotAnimation,
+  loop: true,
+};
+
+const ScrollWithOffsetAndLoop = () => {
+  const lottieObj = useLottie(options, style);
+  const Animation = useLottieInteractivity({
+    lottieObj,
+    mode: "scroll",
+    actions: [
+      {
+        visibility: [0, 0.2],
+        type: "stop",
+        frames: [0],
+      },
+      {
+        visibility: [0.2, 0.45],
+        type: "seek",
+        frames: [0, 45],
+      },
+      {
+        visibility: [0.45, 1.0],
+        type: "loop",
+        frames: [45, 60],
+      },
+    ],
+  });
+
+  return Animation;
+};
+// max 180
+
+export default ScrollWithOffsetAndLoop;
