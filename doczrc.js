b1283cb48c43ae8de7ed97518d647082f76e3388
David Gamote <contact@gamote.ro>
2020-12-07 20:22:27 +0100

    chore: docs moved to old-docs

--- doczrc.js
+++ doczrc.js
@@ -1,7 +1,7 @@
 export default {
   menu: ["Components", "Hooks"],
-  src: "docs",
-  dest: "docs-dist",
+  src: "old-docs",
+  dest: "old-docs-dist",
   base: "/lottie-react/", // GitHub Pages sub-path
   ignore: ["README.md"],
   title: "Lottie for React",
