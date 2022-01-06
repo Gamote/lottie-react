2970c7aaee54dec60b19758f45742eb606824aa5
David Gamote <contact@gamote.ro>
2022-01-06 16:59:04 +0100

    chore: renew the ESLint & Prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  printWidth: 100, // Specify the line length that the printer will wrap on
+  tabWidth: 2, // Specify the number of spaces per indentation-level
+  semi: true, // Print semicolons at the ends of statements
+  trailingComma: "all", // Print trailing commas wherever possible in multi-line comma-separated syntactic structures
+};
