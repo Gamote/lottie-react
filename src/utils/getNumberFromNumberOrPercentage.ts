9a55bdfb2da8fb258102d5d77968fd26d6620d2b
David Gamote <contact@gamote.ro>
2022-01-09 02:52:35 +0100

    fix: `getNumberFromNumberOrPercentage` is failing with float numbers

--- src/utils/getNumberFromNumberOrPercentage.ts
+++ src/utils/getNumberFromNumberOrPercentage.ts
@@ -1,16 +1,16 @@
 /**
  * Extract a number from a number or percentage
+ * TODO: check if there is a more efficient way (RegEX maybe)
  * @param value
  */
 const getNumberFromNumberOrPercentage = (value: string | number) => {
-  const matches = /^(\d+)(%?)$/.exec(String(value));
-
-  console.log(matches);
+  // console.log(value);
+  const matches = /^(([0-9]*[.])?[0-9]+)(%?)$/.exec(String(value));
 
   if (matches) {
     return {
       number: Number(matches[1]),
-      isPercentage: matches[2] === "%",
+      isPercentage: matches[3] === "%",
     };
   }
 
