ac67d8f9e67e8ed2c897533e741517c79a45ac68
David Gamote <contact@gamote.ro>
2022-01-09 16:28:46 +0100

    chore: make website use the 'dark' theme as default

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -36,6 +36,9 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: "dark",
+      },
       navbar: {
         title: "Lottie React",
         logo: {
