347efa3eef3be72b4a40a3d9d48f55318341d406
David Gamote <contact@gamote.ro>
2022-01-15 22:31:05 +0100

    fix: `PlayerControlsProgressBar` is breaking the styling when `_totalFrames` is `0`

--- src/components/player/PlayerControlsProgressBar/PlayerControlsProgressBar.tsx
+++ src/components/player/PlayerControlsProgressBar/PlayerControlsProgressBar.tsx
@@ -10,8 +10,6 @@ export type ProgressBarProps = Pick<LottieHookResult, "totalFrames" | "eventSubs
 /**
  * Component that show the animation's progress and offer ways to
  * select a new frame from `0` to `totalFrames`
- *
- * TODO: deal with the case in witch totalFrames = 0, is breaking the designs
  */
 export const PlayerControlsProgressBar = (props: ProgressBarProps) => {
   const containerRef = useRef<HTMLInputElement>(null);
@@ -57,28 +55,32 @@ export const PlayerControlsProgressBar = (props: ProgressBarProps) => {
   };
 
   return (
-    <input
-      ref={containerRef}
-      className={"player-controls-progress-bar"}
-      type="range"
-      style={{
-        /**
-         * Set the current progress percentage as a CSS var, so it can be used in the style
-         * to properly customize the input range styling across browsers
-         *
-         * Source: https://toughengineer.github.io/demo/slider-styler/slider-styler.html
-         */
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        "--min": 0,
-        "--max": _totalFrames,
-        // "--value": 0,
-      }}
-      onChange={onChangeHandler}
-      onMouseUp={onMouseUpHandler}
-      min={0}
-      max={_totalFrames}
-      step={0.001}
-    />
+    <div style={{ flex: 1 }}>
+      {!!_totalFrames && (
+        <input
+          ref={containerRef}
+          className={"player-controls-progress-bar"}
+          type="range"
+          style={{
+            /**
+             * Set the current progress percentage as a CSS var, so it can be used in the style
+             * to properly customize the input range styling across browsers
+             *
+             * Source: https://toughengineer.github.io/demo/slider-styler/slider-styler.html
+             */
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            "--min": 0,
+            "--max": _totalFrames,
+            // "--value": 0,
+          }}
+          onChange={onChangeHandler}
+          onMouseUp={onMouseUpHandler}
+          min={0}
+          max={_totalFrames}
+          step={0.001}
+        />
+      )}
+    </div>
   );
 };
