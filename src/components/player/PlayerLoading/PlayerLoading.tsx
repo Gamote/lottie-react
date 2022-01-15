ec9a63abf5d752028f97659fec2b2e368f71ac93
David Gamote <contact@gamote.ro>
2022-01-15 22:50:18 +0100

    chore: create a PlayerLoading component

---
+++ src/components/player/PlayerLoading/PlayerLoading.tsx
@@ -0,0 +1,40 @@
+import "./PlayerLoading.less";
+import React, { FC } from "react";
+import PlayerOverlay from "../PlayerOverlay";
+
+export type PlayerLoadingProps = {
+  LoadingComponent?: JSX.Element;
+  color?: string;
+  size?: number;
+};
+
+/**
+ * Component that is displayed when the player is in the Loading state
+ */
+export const PlayerLoading: FC<PlayerLoadingProps> = ({
+  LoadingComponent,
+  color = "#FFFFFF",
+  size = 80,
+}) => {
+  if (LoadingComponent) {
+    return LoadingComponent;
+  }
+
+  const circles = [...Array(2).keys()].map((_, index) => (
+    <div
+      key={index}
+      style={{
+        borderColor: `${color}`,
+        borderWidth: size * 0.05,
+      }}
+    />
+  ));
+
+  return (
+    <PlayerOverlay>
+      <div className={"player-loading-spinner"} style={{ width: size, height: size }}>
+        {circles}
+      </div>
+    </PlayerOverlay>
+  );
+};
