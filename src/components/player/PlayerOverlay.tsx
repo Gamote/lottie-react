5e95c3abca80ef8a70f5b9eb6c62c34f0ad19108
David Gamote <contact@gamote.ro>
2022-01-15 22:35:14 +0100

    chore: add a player overlay to be used by components like `PlayerLoading` or `PlayerError`

---
+++ src/components/player/PlayerOverlay.tsx
@@ -0,0 +1,29 @@
+import React, { DetailedHTMLProps, FC, HTMLAttributes } from "react";
+
+type PlayerOverlayProps = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> & {
+  //
+};
+
+/**
+ * Add a div with padding to the left and right of `size` / 2
+ */
+const PlayerOverlay: FC<PlayerOverlayProps> = ({ children, style, ...rest }) => (
+  <div
+    {...rest}
+    style={{
+      position: "absolute",
+      display: "flex",
+      width: "100%",
+      height: "100%",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "rgba(0,122,135,0.7)",
+      zIndex: 1,
+      ...style,
+    }}
+  >
+    {children}
+  </div>
+);
+
+export default PlayerOverlay;
