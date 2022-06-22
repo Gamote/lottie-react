a6f7caaeba4dc92ca4602088eb35df8eed49f20b
Gamote <contact@gamote.ro>
2022-06-23 00:35:48 +0200

    chore: remove backward compatibility with v1 as planed

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,7 @@
 import LottiePlayer from "lottie-web";
-
 import Lottie from "./components/Lottie";
 import useLottie from "./hooks/useLottie";
 import useLottieInteractivity from "./hooks/useLottieInteractivity";
-
-// TODO: For backward compatibility, remove on v2
-export const Animator: typeof Lottie = Lottie;
-export const useAnimator: typeof useLottie = useLottie;
 
 export { LottiePlayer, useLottie, useLottieInteractivity };
 
